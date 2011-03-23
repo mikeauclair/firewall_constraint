0cc4bb66117d5c3524e324035486b149254d0b58
Mike Auclair <mauclair@nicusa.com>
2011-03-23 11:02:17 -0400

    quick proof of concept spike

--- lib/firewall_constraint.rb
+++ lib/firewall_constraint.rb
@@ -1,6 +1,24 @@
 module FirewallConstraint
   class Constraint
-    
+    def initialize(ips = {})
+      @config = ips.empty? ? 
+        YAML.load_file(Rails.root.join('config','firewall_constraint.yml'))[Rails.env] :
+        ips
+
+      @ips = @config.map{|c| IPAddress::parse(c)}
+    end
+
+    def matches?(request)
+      client_ip = IPAddress::parse(request.env["HTTP_X_FORWARDED_FOR"] ? request.env["HTTP_X_FORWARDED_FOR"] : request.remote_ip)
+      @ips.each do |ip|
+        Rails.logger.warn "#{client_ip.inspect} vs #{ip.inspect}"
+        begin
+          return true if ip.include?(client_ip)
+        rescue NoMethodError => nme
+        end
+      end
+      false
+    end
   end
   # Your code goes here...
 end
